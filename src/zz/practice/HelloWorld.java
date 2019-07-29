7e83142818671dba2505552b6a45d9af347676c7
zhangzhao <1837621988@qq.com>
2019-07-29 15:42:44 +0800

    add a output System.out.println("hello world2.0");

--- src/zz/practice/HelloWorld.java
+++ src/zz/practice/HelloWorld.java
@@ -3,5 +3,6 @@ package zz.practice;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello world1.0");
+        System.out.println("hello world2.0");
     }
 }
